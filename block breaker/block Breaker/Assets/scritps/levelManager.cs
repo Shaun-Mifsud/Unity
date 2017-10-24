91b0bece566b7982a307aea02ae567abeca2e789
Shaun Mifsud <b42068@mcast.org.mt>
2017-10-24 14:41:24 +0200

    new game

---
+++ block breaker/block Breaker/Assets/scritps/levelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string levelName)
+    {
+        print("Loading level " + levelName);
+        //loads the scene named levelName
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void exit()
+    {
+        print("Exiting the game");
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
---
+++ numberWizzard/NumberWizzard/Assets/scritps/NumberWizzard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumberWizzard : MonoBehaviour {
+    //variables
+    int Tries=10;
+    int Min=1;
+    int Max=1001;
+    int guess=500;
+
+
+    public Text guessText;
+
+    void NextGuess()
+    {
+        guessText.text = guess.ToString();
+
+        guess = (Max +Min)/2;
+        Tries --;
+
+        if (Tries == 0)
+        {
+            SceneManager.LoadScene("win");
+        }
+
+        
+    }
+
+   public void LowGuess()
+    {
+        Max = guess;
+        NextGuess();
+    }
+
+   public void HighGuess()
+    {
+        Min = guess;
+        NextGuess();
+    }
+
+
+}
