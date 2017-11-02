a8208eca45f98ad51e5c0435640945510cb14899
Shaun Mifsud <b42068@mcast.org.mt>
2017-11-02 09:45:58 +0100

    lol
    Signed-off-by: Shaun Mifsud <b42068@mcast.org.mt>

---
+++ block breaker/block Breaker/Assets/scritps/LoseCollider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    public levelManager myLevelManager;
+
+
+    //is called when collision happens
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collision happened with "+ collision.gameObject.name);
+        
+    }
+
+    //is called when collider is a trigger
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Triggered by " + collision.gameObject.name);
+        myLevelManager.LoadLevel("win");
+    }
+
+    void Start () {
+		
+
+	}
+	
+
+	void Update () {
+		
+
+	}
+}
